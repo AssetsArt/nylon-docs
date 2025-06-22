476eaec0d9521b651835feb131bd64895d8cd8b0
detoro <meanstack20@gmail.com>
2025-06-22 22:48:07 +0700

    Enhance Astro configuration and documentation for Nylon; added rehype-mermaid integration for improved diagram support, updated title and description, and refined sidebar navigation structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,51 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import cloudflare from '@astrojs/cloudflare';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'Nylon',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AssetsArt/nylon' }],
-          sidebar: [
-              {
-                  label: 'Getting Started',
-                  items: [
-                      { label: 'Installation', slug: 'getting-started/installation' },
-                  ],
-              },
-          ],
-      }),
-	],
+    markdown: {
+        rehypePlugins: [
+            [rehypeMermaid, {
+                strategy: 'img-png',
+                mermaidConfig: {
+                    theme: "dark",
+                    look: 'handDrawn',
+                    handDrawnSeed: 1
+                }
+            }]
+        ],
+    },
+    integrations: [
+        starlight({
+            title: 'Nylon',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AssetsArt/nylon' }],
+            sidebar: [
+                {
+                    label: 'Overview',
+                    slug: 'overview',
+                },
+                {
+                    label: 'Getting Started',
+                    items: [
+                        { label: 'Installation', slug: 'getting-started/installation' },
+                    ],
+                },
+                {
+                    label: 'Plugin System',
+                    items: [
+                        { label: 'Overview', slug: 'plugin-system' },
+                        { label: 'Go', slug: 'plugin-system/go' },
+                        // { label: 'Rust', slug: 'plugin-system/rust/overview' },
+                        // { label: 'Zig', slug: 'plugin-system/zig/overview' },
+                        // { label: 'C', slug: 'plugin-system/c/overview' },
+                    ],
+                },
+            ],
+        }),
+    ],
 
-  adapter: cloudflare(),
+    adapter: cloudflare(),
 });
