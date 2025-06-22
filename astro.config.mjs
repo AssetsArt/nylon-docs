38c17e4b71def15ac94fef1ddfa0972fcca8a4c7
detoro <meanstack20@gmail.com>
2025-06-22 15:18:17 +0700

    Integrate Cloudflare adapter into Astro configuration; updated dependencies and adjusted integration settings for improved deployment support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Nylon',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AssetsArt/nylon' }],
-			sidebar: [
-				{
-					label: 'Getting Started',
-					items: [
-						{ label: 'Installation', slug: 'getting-started/installation' },
-					],
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Nylon',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AssetsArt/nylon' }],
+          sidebar: [
+              {
+                  label: 'Getting Started',
+                  items: [
+                      { label: 'Installation', slug: 'getting-started/installation' },
+                  ],
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
